087fa07f5968ae6061c601e7b62532130297181b
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-15 09:58:15 +0300

    1. Örnek

--- AlgrotimaSorulari/Program.cs
+++ AlgrotimaSorulari/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//İki sayının toplamını veren uygulamayı yazalım
+#region 1. Çözüm
+int sayi1 = 10;
+int sayi2 = 20;
+int sonuc = sayi1 + sayi2;
+Console.WriteLine(sonuc);
+#endregion
+#region 2. Çözüm
+int sayi3 = 10, sayi4 = 20;
+int sonuc2 = sayi3 + sayi4;
+Console.WriteLine(sonuc2);
+#endregion
+#region 3. Çözüm
+Console.WriteLine(sayi1 + sayi2);
+#endregion
