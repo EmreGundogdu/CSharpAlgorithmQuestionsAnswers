13c88bc62dd07cc2adc8850be0608f0b0d9b8e3e
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-02-18 09:05:22 +0300

    2. çözüm

--- AlgrotimaSorulari/Program.cs
+++ AlgrotimaSorulari/Program.cs
@@ -187,3 +187,30 @@
 //int sayi = int.Parse(Console.ReadLine());
 //Console.WriteLine(Faktoriyel(sayi));
 #endregion
+//Pozitif sayılarda çarpma işlemini toplama kullanıarak bulan uygulamayı yazalım
+#region 1. Çözüm
+//Console.WriteLine("Lütfen birinci sayıyı giriniz");
+//int sayi1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Lütfen ikinci sayıyı giriniz");
+//int sayi2 = Convert.ToInt32(Console.ReadLine());
+//int carpmaSonucu = 0;
+//for (int i = 0; i < sayi1; i++)
+//{
+//    carpmaSonucu += sayi2;
+//}
+//Console.WriteLine($"{sayi1} X {sayi2} = {carpmaSonucu}");
+#endregion
+#region 2. Çözüm
+//Console.WriteLine("Lütfen birinci sayıyı giriniz");
+//int sayi1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Lütfen ikinci sayıyı giriniz");
+//int sayi2 = int.Parse(Console.ReadLine());
+//int sayac = sayi2;
+//int sonuc = 0;
+//while (sayac > 0)
+//{
+//    sonuc += sayi1;
+//    sayac--;
+//}
+//Console.WriteLine($"{sayi1} X {sayi2} = {sonuc}");
+#endregion
